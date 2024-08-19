bb3f7992c0dfccdceb61120bd21aec9de3bacdf5
hermlon <hermlon@yuustan.space>
2024-08-19 13:44:15 +0200

    hibernate and swap

--- hosts/natsuki/hardware-configuration.nix
+++ hosts/natsuki/hardware-configuration.nix
@@ -12,6 +12,8 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+	boot.resumeDevice = "/dev/disk/by-uuid/dfd872a1-eb63-46f6-a2cc-7e8ffa7c2d5d";
+	boot.kernelParams = [ "resume_offset=533760" ];
 
   boot.initrd.luks.devices."system".device = "/dev/disk/by-uuid/f59772f1-eda0-4d76-b9db-ed6653743f6e";
 
@@ -45,7 +47,7 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/swap/swapfile"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
